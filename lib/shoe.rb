ce2df409299c0d50a59f9ed9d86953ff41381bfe
Joshua Perez Leduc <joshuapleduc@gmail.com>
2020-04-11 03:21:16 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,16 +6,17 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
-
-  def brand=(brand)
-    @brand = brand
-    BRANDS << brand
-  end
+  # 
+  # def brand=(brand)
+  #   @brand = brand
+  #   BRANDS << brand
+  # end
 
 end
